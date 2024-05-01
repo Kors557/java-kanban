9247056469db1c644a98441d2ec1d8a35025c18f
Kors557 <1500459@mail.ru>
2024-05-01 10:50:50 +0300

    1.3

---
+++ test/ManagersTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import taskmanager.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ManagersTest {
+
+    @Test
+    public void assertEqualsInMemoryHistoryManagerTest() {
+        HistoryManager expected = new InMemoryHistoryManager();
+        HistoryManager actual = Managers.getDefaultHistory();
+        Assertions.assertNotNull(actual, "Объект не был создан.");
+        assertEquals(expected.getHistory(), actual.getHistory(), ", history");
+    }
+
+    @Test
+    public void assertEqualsTaskManagerTest() {
+        TaskManager expected = new InMemoryTaskManager();
+        TaskManager actual = Managers.getDefault();
+        Assertions.assertNotNull(actual, "Объект не был создан.");
+        assertEquals(expected.getAllTask(), actual.getAllTask(), ", tasks");
+    }
+}
